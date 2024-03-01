ed41b998e551fd894626a188360067281c47ee98
Dominique Adolphe <prethora.co@gmail.com>
2024-03-02 00:34:38 +0400

    2024-03-02-00-34

---
+++ config.js
@@ -0,0 +1,23 @@
+export const reddits = {
+    "forhire": {flair: "Hiring"},
+    "hiring": {title: "Hiring"},
+    "Jobs4Bitcoins": {title: "Hiring"},
+};
+
+export const redisUrls = {
+    internal: "redis://red-cngrn1da73kc73c8lcug:6379",
+    external: "rediss://red-cngrn1da73kc73c8lcug:Kq46fxGsxPOnVlPFWGKIQPzQOH3JyvUn@oregon-redis.render.com:6379"
+};
+
+export const getRedisUrl = () => {
+    return (process.env.PLATFORM==="render")?redisUrls.internal:redisUrls.external;
+}
+
+export const maxInRedisStore = 100;
+
+export const telegramConfig = {
+    token: "7081544825:AAFeeurelIuQyngetPSVfpcm3SghNYFil60",
+    chatId: "712317256",
+};
+
+export const httpGetPath = "/uydsfusd7ds7fsydd2y828e";
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// import fetch from "node-fetch";
 import express from "express";
+import { httpGetPath } from "./config.js";
+import { execProcess } from "./lib/process.js";
+import { libRedis } from "./lib/redis.js";
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/",(req,res) => {
+app.get(httpGetPath,(req,res) => {
+    execProcess();
     res.send("ok");
 });
 
+await libRedis.initialize();
 app.listen(port,() => console.log(`listening on port ${port}`));
